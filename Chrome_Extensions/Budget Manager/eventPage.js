c90038e3b686c4b9ac22012d077583713509e7ad
Maham Khan <mahamkh.khan@mail.utoronto.ca>
2020-04-20 21:58:16 -0400

    Add functionality to context menu in budget manager

--- Chrome_Extensions/Budget Manager/eventPage.js
+++ Chrome_Extensions/Budget Manager/eventPage.js
@@ -7,3 +7,33 @@ var contextMenuItem = {
 chrome.runtime.onInstalled.addListener(function() {
     chrome.contextMenus.create(contextMenuItem);
   });
+
+function isInt(value) {
+    return !isNaN(value) && parseInt(Number(value)) == value && !isNaN(parseInt(value, 10));
+}
+
+chrome.contextMenus.onClicked.addListener(function(clickData) { // listening to the click event on the context menu
+    if (clickData.menuItemId == "spendMoney" && clickData.selectionText) {  // if what the user clicked on is our menu item AND there is some selected data
+        if (isInt(clickData.selectionText)) {
+            chrome.storage.sync.get(['total', 'limit'], function(budget){
+                var newTotal = 0;
+                if (budget.total) { // if there is an existing total 
+                    newTotal += parseInt(budget.total);
+                }
+                newTotal += parseInt(clickData.selectionText); // add to this whatever the user selected
+                chrome.storage.sync.set({'total': newTotal}, function() {
+                    if (newTotal >= budget.limit) {
+                        var notifOptions = { 
+                            type: 'basic',
+                            iconUrl: 'icon48.png',
+                            title: 'Limit reached!', 
+                            message: "Uh oh! Looks like ou've reached your limit!"
+                        };
+                        chrome.notifications.create('limitNotif', notifOptions); 
+                    }
+
+                }); chrome.notifications.clear('limitNotif');
+            })
+        }
+    } 
+})
