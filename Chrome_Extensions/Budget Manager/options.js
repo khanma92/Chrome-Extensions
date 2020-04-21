3895b29e38702ec7ee32c69b95bbfb3e7cb8bd45
Maham Khan <mahamkh.khan@mail.utoronto.ca>
2020-04-20 20:36:18 -0400

    Get notifications when new limit saved, when you surpass limit, when you reset total

--- Chrome_Extensions/Budget Manager/options.js
+++ Chrome_Extensions/Budget Manager/options.js
@@ -10,12 +10,31 @@ $(function() {
         var limit = $('#limit').val();
         if(limit) {
             chrome.storage.sync.set({'limit': limit}, function() {
+                var notifOptions = { 
+                    type: 'basic',
+                    iconUrl: 'icon48.png',
+                    title: 'New limit saved!', 
+                    message: "Your new limit has been reset to " + limit.toString()
+                };
+                chrome.notifications.create('savedNotif', notifOptions);
                 close(); // close the tab once limit is set
             })
+
+            chrome.notifications.clear('savedNotif');
         }
     })
 
-    $('resetTotal').click(function() {
-        chrome.storage.sync.set({'total': 0})
+    $('#resetTotal').click(function() {
+        chrome.storage.sync.set({'total': 0}, function() {
+            var notifOptions = { 
+                type: 'basic',
+                iconUrl: 'icon48.png',
+                title: 'Total reset!', 
+                message: "Total has been reset to 0"
+            };
+            chrome.notifications.create('totalNotif', notifOptions); 
+        })
+
+        chrome.notifications.clear('totalNotif');
     })
 })
--- Chrome_Extensions/Budget Manager/popup.js
+++ Chrome_Extensions/Budget Manager/popup.js
@@ -6,7 +6,7 @@ $(function(){
     }) // this gets the previous value that we stored so we don't start from zero each time
 
     $('#spendAmount').click(function(){ // the code below executes once we click on the submit button
-        chrome.storage.sync.get('total', function(budget) {
+        chrome.storage.sync.get(['total', 'limit'], function(budget) { // everytime you need to get more things, add those things in an array
             var newTotal = 0;
             if (budget.total){
                 newTotal += parseInt(budget.total);
@@ -16,9 +16,21 @@ $(function(){
                 newTotal += parseInt(amount);
             }
 
-            chrome.storage.sync.set({'total': newTotal});
+            chrome.storage.sync.set({'total': newTotal}, function() {
+                if (amount && newTotal >= budget.limit){
+                    var notifOptions = { //go over Chrome developer website to review other kinds of options
+                        type: 'basic',
+                        iconUrl: 'icon48.png',
+                        title: 'Limit reached!', 
+                        message: "Uh oh! Looks like you've reached your limit!"
+                    };
+                    chrome.notifications.create('limitNotif', notifOptions); // use chrome API to create notification
+                }
+            });
             $('#total').text(newTotal);
             $('#amount').val('');
         })
+
+        chrome.notifications.clear('limitNotif')
     })
 })
